62af0c02a15c8213d30073314d8922bf7d13c5ef
Ilya Ponetayev <instenet@gmail.com>
2010-08-12 03:26:10 +0400

    Some code cleanups

--- imgps.php
+++ imgps.php
@@ -40,16 +40,11 @@ define("IMAGETYPE_GD2", "102");
 
 function file_getlastslash($str)
 {
-  global $slash;
-  $ps = strpos($str, $slash);
-  if ($ps == FALSE)
-  {
+  global $slash; 
+  if (($ps = strpos($str, $slash)) == FALSE)
     return 0;
-  };
   while (strpos($str, $slash, $ps + 1) != FALSE)
-  {
    $ps = strpos($str, $slash, $ps + 1);
-  };
   return $ps; 
 };
 
@@ -59,9 +54,7 @@ function file_getlastslash($str)
 
 function file_pathtoname($str)
 {
-  $ps = file_getlastslash($str);
-  $s = substr($str, $ps + 1, strlen($str) - $ps);
-  return $s;
+  return substr($str, file_getlastslash($str) + 1, strlen($str) - file_getlastslash($str));
 };
 
 /*
@@ -92,30 +85,25 @@ function imageconvert($infilename, $outfilename, $outformat, $dh, $dw, $quality)
  global $isReadGIF;
  global $OutInter;
  $infmt = exif_imagetype($infilename);
- if ($infmt == IMAGETYPE_JPEG)
- {
+ if ($infmt == IMAGETYPE_JPEG) {
   $image = imagecreatefromjpeg($infilename);
   $IF = "JPEG";
  };
- if ($infmt == IMAGETYPE_PNG)
- {
+ if ($infmt == IMAGETYPE_PNG) {
   $image = imagecreatefrompng($infilename);
   $IF = "PNG";
  };
- if (($infmt == IMAGETYPE_GIF) AND $isReadGIF)
- {
+ if (($infmt == IMAGETYPE_GIF) && $isReadGIF) {
   $image = imagecreatefromgif($infilename);
   $IF = "GIF";
  };
-if ($image != FALSE)
-{
+if ($image != FALSE) {
  $in = getimagesize($infilename);
  $inheight = $in[1];
  $inwidth = $in[2];
  $sw = imagesx($image);
  $sh = imagesy($image);
- if (($dh == -5) AND ($dw == -5))
- {
+ if (($dh == -5) && ($dw == -5)) {
   /*
     Don't resize
   */
@@ -123,71 +111,39 @@ if ($image != FALSE)
    $dw = $sw;
  };
  if ($dh == -1) // Autocomputing size
- { 
    $dh = (int)$dw*($sh/$sw);
- };
  $oimage = imagecreatetruecolor($dw, $dh);
  if ($outformat == -1) // Autocomputing format
- {
   $outformat = $infmt;
- };
- if ($outformat == IMAGETYPE_GIF)
- {
+ if ($outformat == IMAGETYPE_GIF) {
     $OF = "GIF";
-  if ($isWriteGIF)
-  {
-    $OF = "GIF";
-  } else
-  {
+  if (!$isWriteGIF)
     $outformat = IMAGETYPE_JPEG;
-  }; 
  };
  if ($outformat == IMAGETYPE_JPEG)
- {
   $OF = "JPEG";
- };
  if ($outformat == IMAGETYPE_PNG)
- {
   $OF = "PNG";
- };
- if ($outformat == IMAGETYPE_GD)
- {
+ if ($outformat == IMAGETYPE_GD) 
   $OF = "GD";
- };
  if ($outformat == IMAGETYPE_GD2)
- {
   $OF = "GD2";
- };
  if (($OutInter) AND ($outformat != IMAGETYPE_GD2) AND ($outformat != IMAGETYPE_GD))
- {
   $OF = $OF."(i)";
- };
  print("-> Converting '".file_pathtoname($infilename)."' (".$IF." -> ".$OF."; ".$sw."x".$sh." -> ".$dw."x".$dh.")...");
  imagecopyresampled($oimage, $image, 0, 0, 0, 0, $dw, $dh, $sw, $sh);
  if (($OutInter) AND ($outformat != IMAGETYPE_GD2) AND ($outformat != IMAGETYPE_GD))
- {
   imageinterlace($oimage);
- };
  if ($outformat == IMAGETYPE_JPEG)
- {
   imagejpeg($oimage, $outfilename, $quality);
- };
  if ($outformat == IMAGETYPE_PNG)
- {
   imagepng($oimage, $outfilename);
- };
  if ($outformat == IMAGETYPE_GIF)
- {
   imagegif($oimage, $outfilename);
- };
  if ($outformat == IMAGETYPE_GD)
- {
   imagegd($oimage, $outfilename);
- };
  if ($outformat == IMAGETYPE_GD2)
- {
   imagegd2($oimage, $outfilename);
- };
  print("  DONE!".chr(10));
  return 1;
 } else
@@ -205,16 +161,12 @@ function file_preparefilename($out, $format, $name)
 {
   global $PREFIX;
   $newname = "-1";
-  if ($format == $out)
-  {
+  if ($format == $out) {
     $old = file_fulltoshort($name);
     if ($name != $old.$newext)
-    {
       $newname = $old.$newext;
-    } else
-    {
+    else
       $newname = $PREFIX.$old.$newext;
-    };
    };
  return $newname;
 };
@@ -226,80 +178,29 @@ function file_preparefilename($out, $format, $name)
 function file_convert($readname, $OUTFMT, $h, $w, $q, $rmsrc)
 {
 global $PREFIX;
-if (file_exists($readname))
-    {
+if (file_exists($readname)) {
      $name = $readname;
-     if ($OUTFMT != -1)
-     {
+     if ($OUTFMT != -1) {
        if ($OUTFMT == IMAGETYPE_JPEG)
-       {
          $newext = ".jpg";
-         $old = file_fulltoshort($name);
-         if ($name != $old.$newext)
-         {
-           $newname = $old.$newext;
-         } else
-         {
-           $newname = $PREFIX.$old.$newext;
-         };
-       };
        if ($OUTFMT == IMAGETYPE_PNG)
-       {
          $newext = ".png";
-         $old = file_fulltoshort($name);
-         if ($name != $old.$newext)
-         {
-           $newname = $old.$newext;
-         } else
-         {
-           $newname = $PREFIX.$old.$newext;
-         };
-       };
        if ($OUTFMT == IMAGETYPE_GIF)
-       {
          $newext = ".gif";
-         $old = file_fulltoshort($name);
-         if ($name != $old.$newext)
-         {
-           $newname = $old.$newext;
-         } else
-         {
-           $newname = $PREFIX.$old.$newext;
-         };
-       };
        if ($OUTFMT == IMAGETYPE_GD)
-       {
          $newext = ".gd";
-         $old = file_fulltoshort($name);
-         if ($name != $old.$newext)
-         {
-           $newname = $old.$newext;
-         } else
-         {
-           $newname = $PREFIX.$old.$newext;
-         };
-       };
        if ($OUTFMT == IMAGETYPE_GD2)
-       {
          $newext = ".gd2";
          $old = file_fulltoshort($name);
          if ($name != $old.$newext)
-         {
            $newname = $old.$newext;
-         } else
-         {
+         else
            $newname = $PREFIX.$old.$newext;
-         };
-       };
      } else
-     {
        $newname = $PREFIX.$name;
-     }; 
      $rslt = imageconvert($name, $newname, $OUTFMT, $h, $w, $q);
      if ($rmsrc)
-     {
        unlink($name);
-     };
      return $rslt;
     }
     else
@@ -314,58 +215,46 @@ if (file_exists($readname))
 */
 
 list($usec, $sec) = explode(" ",microtime()); $sysstart = ((float)$usec + (float)$sec);
-print("Starting Image Processing Tool @ PHP v 0.2/100808".chr(10));
+print("Starting Image Processing Tool @ PHP v 0.3/120810".chr(10));
 print("Published under GNU GPL v2.".chr(10));
 print("Your PHP version: '".PHP_VERSION."'.".chr(10));
-print("(c) Ponetayev Ilya aka INSTE, 2008.".chr(10));
+print("(c) Ponetayev Ilya aka INSTE, 2008 - 2010.".chr(10));
 print("-> Checking OS version... ");
-if ((PHP_OS == "Linux") OR (PHP_OS == "FreeBSD"))
-{  
+if ((PHP_OS == "Linux") OR (PHP_OS == "FreeBSD")) {  
 $slash = '/';
 print(PHP_OS.", POSIX slash: '".$slash."'.".chr(10));
 };
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
-{ 
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') { 
 $slash = '\\';
 print(PHP_OS.", Win slash: '".$slash."'.".chr(10));
 };
 
 $gi = gd_info();
 if (($gi != FALSE) AND (strpos($gi["GD Version"], "2.0") != FALSE))
-{
-  
-  print("-> Checking GD version... '".$gi["GD Version"]."', passed.".chr(10));
-} else
-{
+   print("-> Checking GD version... '".$gi["GD Version"]."', passed.".chr(10));
+else
   die("-> Your PHP configuration doesn't support GD or version too old!".chr(10));
-};
 print("-> Checking for reading GIF support in GD2...");
-if ($gi["GIF Read Support"])
-{
+if ($gi["GIF Read Support"]) {
   print(" Yes, reading GIF/GD2 works.".chr(10));
   $isReadGIF = TRUE;
-} else
-{
+} else {
   print(" No, not found.".chr(10));
   $isReadGIF = FALSE;
 };
 print("-> Checking for writing GIF support in GD2...");
-if ($gi["GIF Create Support"])
-{
+if ($gi["GIF Create Support"]) {
   print(" Yes, creating GIF/GD2 works.".chr(10));
   $isWriteGIF = TRUE;
-} else
-{
+} else {
   print(" No, not found.".chr(10));
   $isWriteGIF = FALSE;
 };
 print("-> Checking for reading WBMP support in GD2...");
-if ($gi["WBMP Support"])
-{
+if ($gi["WBMP Support"]) {
   print(" Yes, reading WBMP/GD2 works.".chr(10));
   $isReadWBMP = TRUE;
-} else
-{
+} else {
   print(" No, not found.".chr(10));
   $isReadWBMP = FALSE;
 };
@@ -373,27 +262,19 @@ if ($gi["WBMP Support"])
 $types = imagetypes();
 print("-> Checking for support JPEG in GD2...");
 if ($types & IMG_JPEG)
-{
   print(" Yes, JPEG/GD2 works.".chr(10));
-} else
-{
+else
   die(chr(10)."-> You don't have support JPEG in GD2 and PHP!".chr(10));
-};
 print("-> Checking for support PNG in GD2...");
 if ($types & IMG_PNG)
-{
   print(" Yes, PNG/GD2 works.".chr(10));
-} else
-{
+else
   die(chr(10)."-> You don't have support PNG in GD2 and PHP!".chr(10));
-};
 
 print("-> Analyzing command-line arguments...");
 
 if ($argc < 2)
-{
-  die(chr(10).'-> Run "php imgps.php -h" for help in use.'.chr(10)); 
-};
+  die(chr(10).'-> Run "php imgps.php -h" for help in use.'.chr(10));
 
 /*
  Describes output format, if "-j" then JPEG, if "-p" then PNG, 
@@ -408,7 +289,7 @@ $firstcmd = $argv[1];
  For example: "-irl"
 */
 if ($argc > 2)
- { $scndcmd = $argv[2]; };
+ $scndcmd = $argv[2];
 /*
  Describes output size, may be "1000x500" or only _WIDTH_ ("1000"). If you
  specify only width, height will be computed to save proportional
@@ -417,10 +298,9 @@ if ($argc > 2)
  "1024x768:75" or "1024:75".
 */
 if ($argc > 2)
-{ $thrdcmd = $argv[3]; };
+  $thrdcmd = $argv[3];
 
-if (($firstcmd == "-h") OR ($firstcmd == "--help"))
-{
+if (($firstcmd == "-h") OR ($firstcmd == "--help")) {
  print(" DONE!".chr(10)); 
  echo 
 'Usage:
@@ -447,57 +327,41 @@ if (($firstcmd == "-h") OR ($firstcmd == "--help"))
 
 
 if ($firstcmd == "-j")
- { $OUTFMT = IMAGETYPE_JPEG; };
+ $OUTFMT = IMAGETYPE_JPEG; 
 if ($firstcmd == "-p")
- { $OUTFMT = IMAGETYPE_PNG; };
+ $OUTFMT = IMAGETYPE_PNG;
 if ($firstcmd == "-g")
- { 
    if (!$isWriteGIF)
-   { 
      die("-> You can't create GIF files, your PHP/GD2 don't support it!".chr(10));
-   } else
-   {
-      $OUTFMT = IMAGETYPE_GIF;
-   }; 
- };
+   else
+      $OUTFMT = IMAGETYPE_GIF; 
 if ($firstcmd == "-gd")
- {
   $OUTFMT = IMAGETYPE_GD;
- };
 if ($firstcmd == "-gd2")
- {
   $OUTFMT = IMAGETYPE_GD2;
- };
-if (($firstcmd != "-j") AND ($firstcmd != "-p") 
-    AND ($firstcmd != "-g") AND ($firstcmd != "-gd") 
-    AND ($firstcmd != "-gd2"))
- { $OUTFMT = -1; }; // Autocomputing 
+if (($firstcmd != "-j") && ($firstcmd != "-p") 
+    && ($firstcmd != "-g") && ($firstcmd != "-gd") 
+    && ($firstcmd != "-gd2"))
+  $OUTFMT = -1;  // Autocomputing 
  
 
-if ($thrdcmd != "-n")
-{
-if (strpos($thrdcmd, "x") != FALSE)
- {
-  if (strpos($thrdcmd, ":") != FALSE)
-  {
+if ($thrdcmd != "-n") {
+if (strpos($thrdcmd, "x") != FALSE) {
+  if (strpos($thrdcmd, ":") != FALSE) {
    $w = (int)substr($thrdcmd, 0, strpos($thrdcmd, "x"));
    $h = (int)substr($thrdcmd, strpos($thrdcmd, "x") + 1, strpos($thrdcmd, ":"));
    $q = (int)substr($thrdcmd, strpos($thrdcmd, ":") + 1, strlen($thrdcmd));
-  } else
-  {
+  } else {
    $w = (int)substr($thrdcmd, 0, strpos($thrdcmd, "x"));
    $h = (int)substr($thrdcmd, strpos($thrdcmd, "x") + 1, strlen($thrdcmd));
    $q = $QDEF;
   };
- } else
- {
-  if (strpos($thrdcmd, ":") != FALSE) 
-  {
+ } else {
+  if (strpos($thrdcmd, ":") != FALSE)  {
    $w = (int)substr($thrdcmd, 0, strpos($thrdcmd, ":"));
    $q = (int)substr($thrdcmd, strpos($thrdcmd, ":") + 1, strlen($thrdcmd));
    $h = -1; // Autocomputing
-  } else
-  {
+  } else {
    $w = (int)$thrdcmd;
    $h = -1; // Autocomputing
    $q = $QDEF;
@@ -505,12 +369,9 @@ if (strpos($thrdcmd, "x") != FALSE)
 };
 
 if (($q < 0) OR ($q > 100))
-{
   $q = $QDEF;
-};
 
-} else
-{
+} else {
   /*
     Don't resize, convert only.
   */
@@ -522,45 +383,32 @@ if (($q < 0) OR ($q > 100))
   Remove source after image converting
 */
 if (strpos($scndcmd, "r") != FALSE)
-{
   $RemoveSource = TRUE;
-} else
-{
+else
   $RemoveSource = FALSE;
-};
+
 /*
   Interlace output image
 */
 if (strpos($scndcmd, "l") != FALSE)
-{
   $OutInter = TRUE;
-} else
-{
+else
   $OutInter = FALSE;
-};
 
 if (strpos($scndcmd, "f") != FALSE)
-{
  $scndcmd = "-f";
-};
 
 if (strpos($scndcmd, "i") != FALSE)
-{
  $scndcmd = "-i";
-};
 
 print("   DONE!".chr(10));
 
-if ($scndcmd == "-i")
- {
+if ($scndcmd == "-i") {
   print("-> Converting files from cmd-line...".chr(10));
-  for($i = 4;$i < $argc;$i++)
-  {
-    if (file_exists($argv[$i]))
-    {
+  for($i = 4;$i < $argc;$i++) {
+    if (file_exists($argv[$i])) {
      $rslt = file_convert($argv[$i], $OUTFMT, $h, $w, $q, $RemoveSource);
-     if ($rslt == 1)
-     {
+     if ($rslt == 1) {
       $prcnt = (int)((($i - 3) / ($argc - 4)) * 100);
       list($usec, $sec) = explode(" ",microtime());
       $sysstop = ((float)$usec + (float)$sec);
@@ -574,14 +422,11 @@ if ($scndcmd == "-i")
   };
  };
 
-if ($scndcmd == "-f")
-{
- if (file_exists($argv[4]))
- {
+if ($scndcmd == "-f") {
+ if (file_exists($argv[4])) {
   print("-> Converting files from the filelist '".$argv[4]."'...".chr(10));
   $fd = fopen($argv[4], "r");
-  while (!feof($fd))
-  {
+  while (!feof($fd)) {
     $readname = fgets($fd, 1024);
     $readname = substr($readname, 0, strlen($readname) - 1);
     file_convert($readname, $OUTFMT, $h, $w, $q, $RemoveSource);
